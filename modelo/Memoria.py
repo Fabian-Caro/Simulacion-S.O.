9ff19d29ec15b2d056574624e0d39fda284eb695
Fabian Caro <fmcaror@udistrital.edu.co>
2025-01-10 20:58:21 -0500

    fix: interbloqueo

--- modelo/Memoria.py
+++ modelo/Memoria.py
@@ -58,34 +58,69 @@ class Memoria:
             if "" in fila:
                 return True
         return False
+    
+    def calcular_memoria_disponible(self, memoria_virtual):
+        contador = 0
+        for fila in memoria_virtual:
+            for columna in fila:
+                if columna == "":
+                    contador += 1
+        return contador
             
+    def paginas(self, nuevo_proceso):
+        auxiliar = nuevo_proceso.get_tamano_proceso()
+        return (auxiliar + 2 - 1) // 2
+    
+    def generar_nombre_pagina(self, id_proceso, numero_pagina):
+        return f"P{numero_pagina}{id_proceso}"
+ 
+    
+    def agregar_paginas_a_memoria_principal(self, proceso):
+        print(f"Tamaño de memoria: {proceso.get_tamano_proceso()}")
+        agregado_en_principal = 0
+        paginas_en_pricipal = 2
+        id_pagina = 1
+        while agregado_en_principal < paginas_en_pricipal:
+            fila_memoria_principal = random.randint(0, 3)
+            columna_memoria_principal = random.randint(0, 3)
+            
+            if agregado_en_principal < paginas_en_pricipal:
+                if self.memoria_principal[fila_memoria_principal][columna_memoria_principal] == "":
+                    self.memoria_principal[fila_memoria_principal][columna_memoria_principal] = f"P{proceso.get_id_proceso()}{id_pagina}"
+                    agregado_en_principal += 1
+                    id_pagina += 1
+                else:
+                    print(f"Posición ({fila_memoria_principal}, {columna_memoria_principal}) ocupada. Buscando otra posición...")
+        
+        return True
+                    
+    def agregar_paginas_a_memoria_virtual(self, proceso):
+        print(f"Tamaño en virtual: {proceso.get_tamano_proceso()}")
+        valor = self.paginas(proceso)
+        print(f"Paginas a virtual: {valor - 2}")
+        
+        agregado_en_virtual = 0
+        paginas_en_pricipal = 2
+        paginas_en_virtual = valor - paginas_en_pricipal
+        id_pagina = 3
+        while agregado_en_virtual < paginas_en_virtual:
+            fila_memoria_virtual = random.randint(0,7)
+            columna_memoria_virtual = random.randint(0,7)
+            
+            if agregado_en_virtual < paginas_en_virtual:
+                if self.memoria_virtual[fila_memoria_virtual][columna_memoria_virtual] == "":
+                    self.memoria_virtual[fila_memoria_virtual][columna_memoria_virtual] = f"P{proceso.get_id_proceso()}{id_pagina}"
+                    agregado_en_virtual += 1
+                    id_pagina += 1
+                else:
+                    print(f"Posición ({fila_memoria_virtual}, {columna_memoria_virtual}) ocupada. Buscando otra posición...")
+                     
+        return True
     
     def agregar_proceso_aleatorio(self, proceso):
         if not self.memoria_disponible(self.memoria_principal):
             print("No hay espacio disponible en la memoria principal.")
             return False
-        
-        agregado_en_principal = False
-        agregado_en_virtual = False
-        
-        while not (agregado_en_principal and agregado_en_virtual):
-            fila_memoria_principal = random.randint(0, 3)  # Genera un número aleatorio entre 0 y 9
-            columna_memoria_principal = random.randint(0, 3)  # Genera un número aleatorio entre 0 y 9
-            fila_memoria_virtual = random.randint(0,7)
-            columna_memoria_virtual = random.randint(0,7)
-            
-            if not agregado_en_principal and self.memoria_principal[fila_memoria_principal][columna_memoria_principal] == "":  # Verifica si la celda está vacía
-                self.memoria_principal[fila_memoria_principal][columna_memoria_principal] = proceso # Agrega el proceso en la posición elegida
-                agregado_en_principal = True
-                print(f"Proceso {proceso} agregado en la posición ({fila_memoria_principal}, {columna_memoria_principal}).")
-            else:
-                print(f"Posición ({fila_memoria_principal}, {columna_memoria_principal}) ocupada. Buscando otra posición...")
-                
-            if not agregado_en_virtual and self.memoria_virtual[fila_memoria_virtual][columna_memoria_virtual] == "":
-                self.memoria_virtual[fila_memoria_virtual][columna_memoria_virtual] = proceso
-                agregado_en_virtual = True
-                print(f"Proceso {proceso} agregado en la posición ({fila_memoria_virtual}, {columna_memoria_virtual}).")
-            else:
-                print(f"Posición ({fila_memoria_virtual}, {columna_memoria_virtual}) ocupada. Buscando otra posición...")
-                
+        self.agregar_paginas_a_memoria_principal(proceso)
+        self.agregar_paginas_a_memoria_virutal(proceso)                
         return True
--- modelo/Procesos.py
+++ modelo/Procesos.py
@@ -1,14 +1,15 @@
-# from modelo.Recurso import Recurso
 import random
 
 class Procesos:
-    def __init__(self, id_proceso, nombre_proceso, tamano_proceso, recursos_necesarios):
+    def __init__(self, id_proceso, nombre_proceso, tamano_proceso, prioridad, recursos_necesarios,estado):
         self.__id_proceso = id_proceso
         self.__nombre_proceso = nombre_proceso
         self.__tamano_proceso = tamano_proceso
+        self.__prioridad = prioridad
         # self.__recursos_asignados = recursos_asignados
         self.__recursos_necesarios = recursos_necesarios
-        
+        self.estado = estado
+
     def get_id_proceso(self):
         return self.__id_proceso
     
@@ -39,21 +40,12 @@ class Procesos:
         else:
             raise ValueError("Tamaño_proceso debe ser un número.")
         
-    # def agregar_recurso_asignado(self, recurso):
-    #     return self.__recursos_asignados.append(recurso)
+    def get_prioridad(self):
+        return self.__prioridad
     
-    # def get_recursos_asignados(self):
-    #     # Devuelve una lista, asegúrate de que nunca devuelva None
-    #     return self.__recursos_asignados if self.__recursos_asignados is not None else []
-
+    def set_priorida(self,prioridad):
+        self.__prioridad = prioridad
     
-    # def set_recursos_asignados(self, recursos_asignados):
-    #     self.__recursos_asignados = recursos_asignados
-    #     # if isinstance(recursos_asignados, list) and all(isinstance(r, bool) for r in recursos_asignados):
-    #     #     self.__recursos_asignados = recursos_asignados
-    #     # else:
-    #     #     raise ValueError("recursos_asignados debe ser una lista de booleanos")
-        
     def get_nombre_recursos(self):
         return [recurso.get_nombre_recurso() for recurso in self.__recursos_asignados]
     
@@ -63,8 +55,23 @@ class Procesos:
     
     def set_recursos_necesarios(self, recursos_necesarios):
         self.__recursos_necesarios = recursos_necesarios
-
-    def liberar_recursos(self):
+    
+    def get_estado(self):
+        return self.estado
+    
+    def set_estado(self,estado):
+        self.estado = estado
+    
+    def no_pasa_a_bloqueados(self):
+        pasa_a_bloqueado = True
+        recursos_no_disponibles = []
+        for recurso in self.__recursos_necesarios:
+            if recurso.get_proceso() != self and recurso.get_proceso() != None:
+                pasa_a_bloqueado = False
+                recursos_no_disponibles.append(int(recurso.get_id_recurso()))
+        return pasa_a_bloqueado,recursos_no_disponibles
+    
+    def liberar_recursos_L(self):
         recursos_libres = []
         for recurso in self.__recursos_necesarios:
             if random.random() < 0.5:
@@ -76,14 +83,16 @@ class Procesos:
         self.__recursos_necesarios = [r for r in self.__recursos_necesarios if r not in recursos_libres]
         return recursos_libres
 
-    def no_pasa_a_bloqueados(self):
-        recurso_disponible = True
-        recursos_no_disponibles = []
+    def liberar_recursos_B(self):
+        # recursos_libres = []
         for recurso in self.__recursos_necesarios:
-            if recurso.get_proceso() != self and recurso.get_proceso() != None:
-                recurso_disponible = False
-                recursos_no_disponibles.append(int(recurso.get_id_recurso()))
-        return recurso_disponible,recursos_no_disponibles
+            if recurso.get_proceso() == self:
+                if random.random() < 0.5:
+                    recurso.set_proceso(None)
+                    # recursos_libres.append(recurso)
+                    
+        # self.__recursos_necesarios = [r for r in self.__recursos_necesarios if r not in recursos_libres]
+        # return recursos_libres
     
     def liberar_todos_recursos(self):
         for recurso in self.__recursos_necesarios:
